9e1f4bdf4128776a0b90822b10661e10a74cc049
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2017-05-16 23:35:00 +0700

    First task done

--- ls.js
+++ ls.js
@@ -1,10 +1,27 @@
 require('./helper')
 
+const fs = require('fs').promise
+const path = require('path')
+
 async function ls() {
   // Use 'await' inside 'async function's
   console.log('Executing ls function...')
+  const promise = fs.readdir(__dirname)
+  const fileNames = await promise
+  for (const item of fileNames) {
+    const filePath = path.join(__dirname, item)
+    fs.stat(filePath, function (error, stats) {
+      if (error) {
+        throw error
+      }
+      if (stats.isDirectory()) {
+        for (const item of stats) {
 
-  // Your implementation here
+        }
+      }
+    });
+  }
 }
 
 ls()
+
